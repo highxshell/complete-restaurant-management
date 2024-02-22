5256a9c0b714108c4f569283a33e4cc75d39849e
Artem Popov <121538758+highxshell@users.noreply.github.com>
2024-02-22 20:44:36 +0600

    fixed ":" receiver

--- routes/orderItemRouter.go
+++ routes/orderItemRouter.go
@@ -8,8 +8,8 @@ import (
 
 func OrderItemRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.GET("/orderItems", controllers.GetOrderItems())
-	incomingRoutes.GET("/orderItems/:orderItem_id", controllers.GetOrderItem())
+	incomingRoutes.GET("/orderItems/:order_item_id", controllers.GetOrderItem())
 	incomingRoutes.GET("/orderItems-order/:order_id", controllers.GetOrderItemsByOrder())
 	incomingRoutes.POST("/orderItems", controllers.CreateOrderItem())
-	incomingRoutes.PATCH("/orderItems/:orderItem_id", controllers.UpdateOrderItem())
+	incomingRoutes.PATCH("/orderItems/:order_item_id", controllers.UpdateOrderItem())
 }
