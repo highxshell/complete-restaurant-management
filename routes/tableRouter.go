f5b29ec7202833e3ddfee62a6bed24710c5df920
Artem Popov <121538758+highxshell@users.noreply.github.com>
2024-02-22 19:37:23 +0600

    changed missed ":"

--- routes/tableRouter.go
+++ routes/tableRouter.go
@@ -8,7 +8,7 @@ import (
 
 func TableRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.GET("/tables", controllers.GetTables())
-	incomingRoutes.GET("/tables/table_id", controllers.GetTable())
+	incomingRoutes.GET("/tables/:table_id", controllers.GetTable())
 	incomingRoutes.POST("/tables", controllers.CreateTable())
-	incomingRoutes.PATCH("/tables/table_id", controllers.UpdateTable())
+	incomingRoutes.PATCH("/tables/:table_id", controllers.UpdateTable())
 }
