3d4dd3ddd99a3e1db5fd20dd3b732830824f8a0b
Artem Popov <121538758+highxshell@users.noreply.github.com>
2024-02-22 20:45:22 +0600

    fixed ":" receiver

--- controllers/orderItemsController.go
+++ controllers/orderItemsController.go
@@ -46,10 +46,10 @@ func GetOrderItem() gin.HandlerFunc {
 		orderItemId := c.Param("order_item_id")
 		var orderItem models.OrderItem
 
-		err := orderItemCollection.FindOne(ctx, bson.M{"orderItem_id": orderItemId}).Decode(&orderItem)
+		err := orderItemCollection.FindOne(ctx, bson.M{"order_item_id": orderItemId}).Decode(&orderItem)
 		defer cancel()
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing oreder item"})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing order item"})
 			return
 		}
 		c.JSON(http.StatusOK, orderItem)
